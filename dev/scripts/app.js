c81cd390bc97c949c4c1087f8a6bcc0b93136b91
Cédric <cedced19@gmail.com>
2014-12-08 21:02:20 +0100

    Adding css

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,4 +1,8 @@
-angular.module("ForkLabel", [])
-.controller("ForkLabelCtrl", ["$scope", function($scope) {
-
+angular.module('ForkLabel', [])
+.controller('ForkLabelCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('config.json').success(function(data) {
+        
+    }).error(function() {
+        $scope.error = true;
+    });
 }]);
